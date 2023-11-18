0951b34ea2b0dd167e138bd3d6ef0de07929c710
hazashbaig <hazashbaig@gmail.com>
2023-11-18 12:23:54 +0530

    added home page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from "./components/Navbar";
 import Artworks from "./pages/Artworks";
+import Home from "./pages/Home";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
           <div className="App">
           <Navbar/>
           <Routes>
+          <Route path="/" Component={Home} />
             <Route path="/artworks" Component={Artworks} />
           </Routes>
           </div>
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import "./Home.css";
+
+function Home () {
+    return (
+        <div className="main">
+            <div className="title">
+                <h1>Art Gallery</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
